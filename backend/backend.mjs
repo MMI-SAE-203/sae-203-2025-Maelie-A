4e6d526e8edc5070e5f2f811dbd353d7db74a749
MaëlieA <maelie@addario.fr>
2025-03-13 15:04:59 +0100

    commit

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
-const pb = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase("https://pb-sae-203.maelie-addario.fr:443");
 
 //routes dynaliques pour les films
 
